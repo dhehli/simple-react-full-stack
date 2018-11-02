447305737f4de85f54c0210386edcec4650ee2c6
dhehli <dh@netlive.ch>
2018-11-02 16:32:04 +0100

    add proper lint config

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.get('/api/list', (req, res) => {
-    res.json({list: "entries"})
-})
+	res.json({ list: 'entries' });
+});
 
 app.listen(8080, () => console.log('Listening on port 8080!'));

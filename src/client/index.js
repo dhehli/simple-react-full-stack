c46fe63af9d6b20068f1385fd942d8f57781da7e
dhehli <dh@netlive.ch>
2018-11-13 16:17:54 +0100

    add

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { IntlProvider } from 'react-intl';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render((
-	<IntlProvider>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</IntlProvider>
 ), document.getElementById('root'));
